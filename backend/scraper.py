56e6c3daee9ec2075a72d8b81f662f4df12fba47
Kesav Kosana <kkosana33@gmail.com>
2022-12-16 17:24:58 -0500

    add distance to UCLA for each apartments from existing google maps API call

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,7 +1,33 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
+import googlemaps
+import requests
+import os
+from dotenv import load_dotenv
 
 # to update frontend: clear apartment_data.json, cd into backend, and run: scrapy runspider scraper.py -o ../frontend/src/data/apartment_data.json
+
+try:
+    # GOOGLE MAPS API SETUP:
+    # Import and set up Google Maps API
+    load_dotenv()
+    GMAPS_API_KEY = os.getenv('GMAPS_API_KEY')
+    gmaps = googlemaps.Client(key=GMAPS_API_KEY)
+
+    # Set up parameters for API Call
+    ucla_address = "405 Hilgard Ave, Los Angeles, CA 90095"
+    base_url = "https://maps.googleapis.com/maps/api/geocode/json"
+
+    # Make API request
+    endpoint = f"{base_url}?address={ucla_address}&key={GMAPS_API_KEY}"
+
+    # Get coordinates of UCLA
+    result_coordinate = (requests.get(endpoint)).json()[
+        'results'][0]['geometry']['location']
+    ucla_lat = result_coordinate['lat']
+    ucla_lng = result_coordinate['lng']
+except Exception as e:
+    print(e)
 
 
 class ApartmentsSpider(scrapy.Spider):
@@ -45,7 +71,11 @@ class ApartmentsSpider(scrapy.Spider):
         close_paren = image_styling.rfind(")")
 
         if open_paren != -1 and close_paren != -1:
-            image_url = image_styling[open_paren + 2:close_paren - 1] # remove one past parantheses to remove opening and closing quotes
+            # remove one past parantheses to remove opening and closing quotes
+            image_url = image_styling[open_paren + 2:close_paren - 1]
+
+        # get distance from UCLA
+        distance = get_distance_from_ucla(address)
 
         # scrape all other details
         details_list = response.xpath(
@@ -60,13 +90,43 @@ class ApartmentsSpider(scrapy.Spider):
             if label == "Monthly Rent":
                 rent = value
             elif label == "Bedrooms":
-                beds = (" ").join(value.split()[:-1])
+                if value and len(value) > 1:
+                    beds = (" ").join(value.split()[:-1])
             elif label == "Bathrooms":
-                baths = (" ").join(value.split()[:-1])
+                if value and len(value) > 1:
+                    baths = (" ").join(value.split()[:-1])
             elif label == "Square Feet":
-                sqft = (" ").join(value.split()[:-2])
+                if value and len(value) > 1:
+                    sqft = (" ").join(value.split()[:-2])
 
-        yield {"name": name, "url": url, "address": address, "beds": beds, "baths": baths, "sqft": sqft, "rent": rent, "image_url": image_url}
+        yield {"name": name, "url": url, "address": address, "beds": beds, "baths": baths, "sqft": sqft, "rent": rent, "image_url": image_url, "distance": distance}
+
+
+def get_distance_from_ucla(address):
+    '''
+    Returns the distance from UCLA in miles
+    Returns -1 if address is invalid or if there is an error
+    '''
+    try:
+        endpoint = f"{base_url}?address={address}&key={GMAPS_API_KEY}"
+
+        # Get coordinates of apartment
+        result_coordinate = (requests.get(endpoint)).json()[
+            'results'][0]['geometry']['location']
+        apartment_lat = result_coordinate['lat']
+        apartment_lng = result_coordinate['lng']
+        origin = (ucla_lat, ucla_lng)
+        destination = (apartment_lat, apartment_lng)
+
+        # Get distance between coords and then convert to miles
+        result_distance = gmaps.distance_matrix(origin, destination, mode='walking')[
+            "rows"][0]["elements"][0]["distance"]["value"]
+        result_distance = 0.621371 * result_distance/1000
+        result_distance = round(result_distance, 2)
+
+        return result_distance
+    except:
+        return -1
 
 
 # uncomment this code and run python scraper.py to run the scraper as a standalone script
